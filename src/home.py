a667a18e7532c120b2e6692950009e7b521430f0
alinaqiu2333 <alinaqiu2333@gmail.com>
2023-05-26 12:30:39 -0400

    unnecessary files removed

--- src/home.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-# create a blueprint obj called home
-home = Blueprint('welcome', __name__)
-
-@home.route('/')
-def home_page():
-    return render_template("home.html")

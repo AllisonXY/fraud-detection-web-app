4b83f8d0438f159fac4b9770dac4baddb350bd9e
seiftobgy1999 <seif19991@gmail.com>
2023-05-31 11:55:35 -0400

    Fixes to form

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -17,13 +17,13 @@ const Contact = () => {
     }
 
     return (
-        <form>
-            <h1>Contact <span>Here</span> </h1>
-            <input type="text" name="name" id ="" onChange={handleChange} value={data.name} placeholder="Enter name"/> 
-            <input type="email" name="email" id ="" onChange={handleChange} value={data.email} placeholder="Enter email"/>  
-            <input type="phone" name="phone" id ="" onChange={handleChange} value={data.phone} placeholder="Enter phone number"/>  
-            <textarea name="text" id ="" cols="30" onChange={handleChange} value={data.message} rows="10" placeholder="Enter message"></textarea>
-            <button type="submit"> Submit </button>
+        <form className="for">
+            <h1>Contact <span className = "formspa">Here</span> </h1>
+            <input className = "forminp" type="text" name="name" id ="" onChange={handleChange} value={data.name} placeholder="Enter name"/> 
+            <input className = "forminp" type="email" name="email" id ="" onChange={handleChange} value={data.email} placeholder="Enter email"/>  
+            <input className = "forminp" type="phone" name="phone" id ="" onChange={handleChange} value={data.phone} placeholder="Enter phone number"/>  
+            <textarea className = "forminp" name="text" id ="" cols="30" onChange={handleChange} value={data.message} rows="10" placeholder="Enter message"></textarea>
+            <button className = "formbut" type="submit"> Submit </button>
         </form>
     )
 }

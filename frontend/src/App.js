754ed4126f8c5ad22ee0e711723e930f3f011971
Kiran Sahmbi <kiransahmbi@yahoo.ca>
2023-06-02 12:35:58 -0400

    Removing Bootstrap Imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactRouter from "./Router";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery/dist/jquery.min.js';
-import 'bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
 
 export default function App() {
